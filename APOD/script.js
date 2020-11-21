e518e6a0624fce602c8abc2293be42064b119301
Daos7623 <augusto.torres@al.resilia.com.br>
2020-11-20 23:12:51 -0300

    Commit da APOD

---
+++ APOD/script.js
@@ -0,0 +1,36 @@
+let requisicao = new XMLHttpRequest();
+let form = document.querySelector('.valor');
+let input = document.querySelector('#date');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    requisicao.open('GET', `https://api.nasa.gov/planetary/apod?api_key=HxfsjMvsjh9PJ6dPJ40ikaqugjUIAJgQbjK8QVN3&date=${input.value}`);
+    requisicao.send();
+});
+
+function newDATE(value) {
+    return value.date.split("-").reverse().join("-");
+}
+requisicao.open("GET", `https://api.nasa.gov/planetary/apod?api_key=HxfsjMvsjh9PJ6dPJ40ikaqugjUIAJgQbjK8QVN3`);
+requisicao.onload = function () {
+
+    let result = JSON.parse(requisicao.responseText);
+    console.log(result);
+
+    let titulo = document.getElementById("t1")
+
+    let dateApod = document.getElementById("date")
+
+    let imgApod = document.getElementById("img")
+
+    let expApod = document.getElementById("exp")
+
+    let copyApod = document.getElementById("copy")
+
+    titulo.textContent = result.title;
+    dateApod.textContent = newDATE(result);
+    imgApod.src = result.url;
+    expApod.textContent = `Explanation: ${result.explanation}`;
+    copyApod.textContent = `Copyright: ${result.copyright}`;
+}
+requisicao.send();
